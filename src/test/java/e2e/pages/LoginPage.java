40b3ad679f70ade84c62e10fb0f5d3ff85fef4ba
OLEKSANDR RASHEVCHENKO <rashevchenkoo@gmail.com>
2023-11-17 19:40:09 +0100

    Added class Locators.java and move allFindElement to this class

---
+++ src/test/java/e2e/pages/LoginPage.java
@@ -0,0 +1,32 @@
+package e2e.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage {
+    // important constructor!!
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    //describe locator
+
+    @FindBy(xpath = "//*[@name = 'email']")
+    WebElement emailImput;
+    @FindBy(xpath = "//*[@name = 'password']")
+    WebElement passwordImput;
+    @FindBy(xpath = "//*[@type='submit']")
+    WebElement loginButton;
+
+
+    //describe methods
+
+    public void login(String email, String password) {
+        emailImput.sendKeys(email);
+        passwordImput.sendKeys(password);
+        loginButton.click();
+    }
+
+
+}
