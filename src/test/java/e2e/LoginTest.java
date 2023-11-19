aed1d99644dea74e5fdef61d4fa15c0e1426dc8f
OLEKSANDR RASHEVCHENKO <rashevchenkoo@gmail.com>
2023-11-19 18:26:16 +0100

    Described userCannotLoginWithInvalidPassword test

--- src/test/java/e2e/LoginTest.java
+++ src/test/java/e2e/LoginTest.java
@@ -2,6 +2,7 @@ package e2e;
 
 import e2e.pages.ContactsPage;
 import e2e.pages.LoginPage;
+import org.checkerframework.checker.units.qual.C;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -31,4 +32,15 @@ public class LoginTest extends TestBase {
         contactsPage = new ContactsPage(app.driver);
         Assert.assertFalse(contactsPage.confirmLogin(), "User is logged");
     }
+    @Test
+    public void userCannotLoginWithInvalidPassword(){
+        String email = "newtest@gmail.com";
+        String password = "newtest@gmail.com";
+
+        loginPage = new LoginPage(app.driver);
+        loginPage.login(email,password);
+        contactsPage = new ContactsPage(app.driver);
+        Assert.assertFalse(contactsPage.confirmLogin(),"User is logged");
+    }
+
 }
