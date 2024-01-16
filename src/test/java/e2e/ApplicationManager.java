134c0596dbb81f46a9a927482a603b1007925d07
LeonadO555 <lmikhailov@supplyshift.net>
2024-01-17 01:38:50 +0400

    Update

--- src/test/java/e2e/ApplicationManager.java
+++ src/test/java/e2e/ApplicationManager.java
@@ -13,7 +13,7 @@ public class ApplicationManager {
     protected void init(){
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
+//        options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("start-maximized");
