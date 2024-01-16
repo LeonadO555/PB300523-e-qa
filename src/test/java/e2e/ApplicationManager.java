768bf80d532c5738b4947b7c83af13586946b895
LeonadO555 <lmikhailov@supplyshift.net>
2024-01-17 02:21:20 +0400

    Update

--- src/test/java/e2e/ApplicationManager.java
+++ src/test/java/e2e/ApplicationManager.java
@@ -3,23 +3,32 @@ package e2e;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.Duration;
 
 public class ApplicationManager {
     public WebDriver driver;
 
-    protected void init(){
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--remote-debugging-port=9222");
-        options.addArguments("start-maximized");
-        driver = new ChromeDriver(options);
+    protected void init(boolean useSelenoid) throws MalformedURLException {
+        if (useSelenoid) {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName("chrome");
+            capabilities.setVersion("latest");
+            capabilities.setCapability("enableVNC", true);
+
+            driver = new RemoteWebDriver(
+                URI.create("http://localhost:4444/wd/hub").toURL(),
+                capabilities
+            );
+        } else {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+
         driver.get("http://phonebook.telran-edu.de:8080/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
