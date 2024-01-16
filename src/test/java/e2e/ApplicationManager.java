e393882c5c335eeba636e17a5c7873b18fd4c202
LeonadO555 <lmikhailov@supplyshift.net>
2024-01-17 03:00:02 +0400

    Update

--- src/test/java/e2e/ApplicationManager.java
+++ src/test/java/e2e/ApplicationManager.java
@@ -21,7 +21,7 @@ public class ApplicationManager {
             capabilities.setCapability("enableVNC", true);
 
             driver = new RemoteWebDriver(
-                URI.create("http://165.227.145.48:4444/wd/hub").toURL(),
+                URI.create("http://localhost:4444/wd/hub").toURL(),
                 capabilities
             );
         } else {
