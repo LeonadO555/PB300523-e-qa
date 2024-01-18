bbf83dce406d7b39a94dc7e0d97f7bc708edae20
Pislaru <pislaru.daniel1@gmail.com>
2024-01-18 20:57:23 +0100

    18.01 Email Api ne dodelal

--- src/test/java/integration/EmailApiTest.java
+++ src/test/java/integration/EmailApiTest.java
@@ -2,15 +2,33 @@ package integration;
 
 import integration.contact.ContactApi;
 import integration.email.EmailApi;
+import integration.schemas.EmailDto;
 import integration.user.UserApi;
 import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class EmailApiTest {
 
     UserApi userApi;
     ContactApi contactApi;
     EmailApi emailApi;
+
+    private void checkEmailData(int phoneId, EmailDto phoneData) {
+        JsonPath actualObject = emailApi.getEmail(200, phoneId).jsonPath();
+        LinkedHashMap<String, String> emailObjects = new LinkedHashMap<>();
+//        phoneObjects.put(actualObject.getString("countryCode"), phoneData.getCountryCode());
+//        phoneObjects.put(actualObject.getString("phoneNumber"), phoneData.getPhoneNumber());
+
+        for (Map.Entry<String, String> phoneObject : emailObjects.entrySet()) {
+            String actualResult = phoneObject.getKey();
+            String expectedResult = phoneObject.getValue();
+            Assert.assertEquals(actualResult, expectedResult, actualResult + " is not equals " + expectedResult);
+        }
+    }
 
     @Test
     public void userCanWorkWithEmailApiTest() {
@@ -25,7 +43,16 @@ public class EmailApiTest {
         int contactId = object.getInt("id");
 
         //create email
+        emailApi = new EmailApi(token);
         emailApi.createEmail(201, contactId);
+        JsonPath emailObject = emailApi.getAllEmails(200, contactId).jsonPath();
+        int emailId = emailObject.getInt("[0].id");
+//        checkEmailData(emailId, emailApi.rndDataForCreateEmailAddress(contactId));
+
+        emailApi.editNewEmail(200, contactId, emailId);
+//        checkEmailData(emailId, emailApi.rndDataForCreateEmailAddress(emailId, contactId));
+//
+//        emailApi.deleteEmail(200, emailId);
 
 
     }
--- src/test/java/integration/email/EmailApi.java
+++ src/test/java/integration/email/EmailApi.java
@@ -15,20 +15,49 @@ public class EmailApi extends ApiBase {
     EmailDto dto;
 
     Faker faker = new Faker();
-    String email = faker.internet().emailAddress();
+    //    String email = faker.internet().emailAddress();
+//    String editEmail = faker.internet().emailAddress();
+    String email = "jorik@mail.ru";
+    String editEmail = "1jorik@mail.ru";
+
 
     public EmailDto rndDataForCreateEmailAddress(int contactId) {
         dto = new EmailDto();
         dto.setEmail(email);
-        dto.setContactID(contactId);
+        dto.setContactId(contactId);
         return dto;
     }
 
-    public Response createEmail(int code, int contactId) {
+    public EmailDto rndDataForEditEmailAddress(int id, int contactId) {
+        dto = new EmailDto();
+        dto.setId(id);
+        dto.setEmail(editEmail);
+        dto.setContactId(contactId);
+        return dto;
+
+    }
+
+    public void createEmail(int code, int contactId) {
         String endpoint = "/api/email";
         Object body = rndDataForCreateEmailAddress(contactId);
         response = postRequest(endpoint, code, body);
-        response.as(EmailDto.class);
+    }
+
+    public Response getAllEmails(int code, int contactId) {
+        String endpoint = "/api/email/{contactId}/all";
+        response = getRequestWithParam(endpoint, code, "contactId", contactId);
+        return response;
+    }
+
+    public void editNewEmail(int code, int contactId, int id) {
+        String endpoint = "/api/email";
+        Object body = rndDataForEditEmailAddress(id, contactId);
+        putRequest(endpoint, code, body);
+    }
+
+    public Response getEmail(int code, int id) {
+        String endpoint = "/api/email/{id}";
+        response = getRequestWithParam(endpoint, code, "id", id);
         return response;
     }
 }
