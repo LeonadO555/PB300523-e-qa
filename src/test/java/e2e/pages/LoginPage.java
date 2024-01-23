68c3d7bd047729f8591616b72100b4ab03ef3f2f
AliaksandrPakhamovich <alexberr9501@gmail.com>
2024-01-23 20:33:02 +0100

    Added @Step

--- src/test/java/e2e/pages/LoginPage.java
+++ src/test/java/e2e/pages/LoginPage.java
@@ -1,5 +1,6 @@
 package e2e.pages;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -22,6 +23,7 @@ public class LoginPage extends BasePage{
     @FindBy(xpath = "//*[@type='submit']")
     WebElement loginButton;
 
+    @Step("Wait for loading login page")
     public void waitForLoading(){
         getWait().forVisibility(emailInput);
         getWait().forVisibility(passwordInput);
