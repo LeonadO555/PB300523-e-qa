5e1cec04fd41da414eb41521ca2ab2fe05aaf83e
Georgiy <g.manolov@web.de>
2024-01-24 11:01:46 +0100

    new

--- src/test/java/e2e/ApplicationManager.java
+++ src/test/java/e2e/ApplicationManager.java
@@ -1,6 +1,7 @@
 package e2e;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -40,7 +41,7 @@ public class ApplicationManager {
         driver = new ChromeDriver();
         }
         driver.get("http://phonebook.telran-edu.de:8080/");
-        driver.manage().window().maximize();
+        driver.manage().window().setSize(new Dimension(1920,1080));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
     protected void stop(){
