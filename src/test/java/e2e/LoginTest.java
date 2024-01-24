3661ac7c1edd0fb0dfa9c2a6d904d41e917322aa
OLEKSANDR RASHEVCHENKO <rashevchenkoo@gmail.com>
2024-01-24 23:14:52 +0100

    added config ,updated screenshot method

--- src/test/java/e2e/LoginTest.java
+++ src/test/java/e2e/LoginTest.java
@@ -22,15 +22,15 @@ public class LoginTest extends TestBase {
         contactsPage.waitForLoading();
     }
 
-//    @Test(dataProvider = "invalidLoginData", dataProviderClass = DataProviders.class)
-//    public void userCannotLogin(String email, String password, String caseName) {
-//        loginPage = new LoginPage(app.driver);
-//        loginPage.waitForLoading();
-//        loginPage.login(email, password);
-//
-//        loginPage.waitForLoading();
+    @Test(dataProvider = "invalidLoginData", dataProviderClass = DataProviders.class)
+    public void userCannotLogin(String email, String password, String caseName) {
+        loginPage = new LoginPage(app.driver);
+        loginPage.waitForLoading();
+        loginPage.login(email, password);
+
+        loginPage.waitForLoading();
 //        loginPage.takeLoginPageScreenshot(caseName + "_negative_login_case");
-//    }
+    }
 
     private void loginTestMethod(String email, String password, String screenshotName, boolean negativeCase) {
         loginPage = new LoginPage(app.driver);
@@ -38,11 +38,11 @@ public class LoginTest extends TestBase {
         loginPage.login(email, password);
         if (negativeCase) {
             loginPage.waitForLoading();
-            loginPage.takeLoginPageScreenshot(screenshotName);
+//            loginPage.takeLoginPageScreenshot(screenshotName);
         } else {
             contactsPage = new ContactsPage(app.driver);
             contactsPage.waitForLoading();
-            contactsPage.takeScreenshotHeader();
+//            contactsPage.takeScreenshotHeader();
         }
     }
 
