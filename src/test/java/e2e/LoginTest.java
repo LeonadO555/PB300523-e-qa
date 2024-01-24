173abb5e5c53e89bea4deeb2c979e8b545985bd9
nata757 <107883642+nata757@users.noreply.github.com>
2024-01-24 17:41:02 +0100

    Added config, updated Screenshot metod, hartcoded window size

--- src/test/java/e2e/LoginTest.java
+++ src/test/java/e2e/LoginTest.java
@@ -3,6 +3,7 @@ package e2e;
 import e2e.pages.ContactsPage;
 import e2e.pages.LoginPage;
 import e2e.utils.DataProviders;
+import io.qameta.allure.*;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -10,8 +11,12 @@ public class LoginTest extends TestBase {
     LoginPage loginPage;
     ContactsPage contactsPage;
 
-
-    @Test
+    @Epic(value = "Login")
+    @Feature(value = "User login")
+    @Story(value = "User can login with role admin")
+    @Description(value = "User can login with role admin")
+    @Severity(SeverityLevel.BLOCKER)
+    @Test(description = "User can login")
     public void userCanLogin() {
         String email = "newtest@gmail.com";
         String password = "newtest@gmail.com";
@@ -37,7 +42,7 @@ public class LoginTest extends TestBase {
     }
 
     @Test(dataProvider = "invalidLoginData", dataProviderClass = DataProviders.class)
-    public void userCannotWithInvalidPassword(String email,String password, String caseName) {
+    public void userCannotWithInvalidPassword(String email, String password, String caseName) {
         loginPage = new LoginPage(app.driver);
         loginPage.waitForLoading();
         loginPage.login(email, password);
@@ -45,14 +50,14 @@ public class LoginTest extends TestBase {
         loginPage.takeLoginPageScreenshot(caseName + "_negative_Login_Case");
     }
 
-    private void loginTestMethod (String email, String password, String screenshotName, boolean negativeCase){
+    private void loginTestMethod(String email, String password, String screenshotName, boolean negativeCase) {
         loginPage = new LoginPage(app.driver);
         loginPage.login(email, password);
 
-        if(negativeCase) {
+        if (negativeCase) {
             loginPage.waitForLoading();
             loginPage.takeLoginPageScreenshot(screenshotName);
-        }else {
+        } else {
             contactsPage = new ContactsPage((app.driver));
             contactsPage.waitForLoading();
             contactsPage.takeScreeshotHeader();
@@ -60,22 +65,24 @@ public class LoginTest extends TestBase {
     }
 
     @Test
-    public void userCanLoginWithValidData(){
+    public void userCanLoginWithValidData() {
         loginTestMethod("newtest@gmail.com", "newtest@gmail.com", null, false);
     }
-// TODO: there is a bug ....
+
+    // TODO: there is a bug ....
     @Test
 
-    public void userCanLoginWithInvalidEmail(){
+    public void userCanLoginWithInvalidEmail() {
         loginTestMethod("newtestgmail.com", "newtest@gmail.com", "login_invalid_email", true);
     }
 
     @Test
-    public void userCanLoginWithInvalidPassword(){
+    public void userCanLoginWithInvalidPassword() {
         loginTestMethod("newtest@gmail.com", "newtestgmail.com", "login_invalid_password", true);
     }
+
     @Test
-    public void userCanLoginWithInvalidEmailAndPassword(){
-        loginTestMethod("newtest@gmail.com", "newtestgmail.com","login_invalid_email_and_password", true);
+    public void userCanLoginWithInvalidEmailAndPassword() {
+        loginTestMethod("newtest@gmail.com", "newtestgmail.com", "login_invalid_email_and_password", true);
     }
 }
