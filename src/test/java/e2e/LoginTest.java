e4d25aa1ed9a1149940baf90c771006e87a73622
Georgiy <g.manolov@web.de>
2024-01-24 17:58:03 +0100

    Added config, updated screenshot, hardcoded window size

--- src/test/java/e2e/LoginTest.java
+++ src/test/java/e2e/LoginTest.java
@@ -26,9 +26,9 @@ public class LoginTest extends TestBase {
         loginPage = new LoginPage(app.driver);
         loginPage.waitForLoading();
         loginPage.login(email, password);
-        loginPage.waitForLoading();
-        loginPage.takeLoginPageScreenshot( caseName + "negativeLoginCase");
 
+        loginPage.waitForLoading();
+        loginPage.takeLoginPageScreenshot(caseName + "_negative_login_case");
     }
 
     private void loginTestMethod(String email, String password, String screenshotName, boolean negativeCase) {
@@ -41,11 +41,8 @@ public class LoginTest extends TestBase {
         } else {
             contactsPage = new ContactsPage(app.driver);
             contactsPage.waitForLoading();
-            //contactsPage.takeScreenshotHeader();
+            contactsPage.takeScreenshotHeader();
         }
-
-        contactsPage = new ContactsPage(app.driver);
-        contactsPage.getWait().forInvisibility(contactsPage.header);
     }
     @Test
     public void userCannotLoginWithInvalidPassword(){
