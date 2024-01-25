433473760ad580faa8a36b989f78f60b9e1306b4
Georgiy <g.manolov@web.de>
2024-01-25 21:24:51 +0100

    Added config, updated screenshot, hardcoded window size

--- src/test/java/e2e/pages/EditPhoneDialog.java
+++ src/test/java/e2e/pages/EditPhoneDialog.java
@@ -1,11 +1,10 @@
 package e2e.pages;
 
-import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class EditPhoneDialog extends PhonesPage {
+public class EditPhoneDialog extends PhoneInfoPage {
     public EditPhoneDialog(WebDriver driver) {
         super(driver);
     }
@@ -14,25 +13,27 @@ public class EditPhoneDialog extends PhonesPage {
     WebElement countryCodeDropDown;
     @FindBy(xpath = "//*[@id='selected-cc']")
     WebElement phoneInputField;
+
     @FindBy(xpath = "//*[@class='btn btn-primary']")
     WebElement saveButton;
 
-    @Step("Wait for open edit phone dialog")
+
     public void waitForOpen() {
         getWait().forVisibility(countryCodeDropDown);
         getWait().forVisibility(phoneInputField);
         getWait().forVisibility(saveButton);
         getWait().forClickable(saveButton);
+
     }
-    @Step("Select country :{country}")
+
     public void selectCountryCode(String code){
         getSelect(countryCodeDropDown).selectByVisibleText(code);
     }
-    @Step
+
     public void setEditPhone(String number) {
         setInput(phoneInputField, number);
     }
-    @Step
+
     public void savePhoneChanges() {
         saveButton.click();
         getWait().forInvisibility(saveButton);
