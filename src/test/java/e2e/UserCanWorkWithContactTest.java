45ee0385b73244adae406982ea94d2e839c5bf81
Georgiy <g.manolov@web.de>
2024-01-28 19:33:12 +0100

    Added config, updated screenshot, hardcoded window size

--- src/test/java/e2e/UserCanWorkWithContactTest.java
+++ src/test/java/e2e/UserCanWorkWithContactTest.java
@@ -4,11 +4,17 @@ import com.github.javafaker.Faker;
 import e2e.pages.*;
 import e2e.pages.contact.*;
 import e2e.utils.DataProviders;
+import integration.contact.ContactApi;
+import integration.user.UserApi;
+import io.qameta.allure.*;
+import io.restassured.path.json.JsonPath;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class UserCanWorkWithContactTest extends TestBase {
     LoginPage loginPage;
+    UserApi userApi;
+    ContactApi contactApi;
     ContactsPage contactsPage;
     AddContactDialog addContactDialog;
     ContactInfoPage contactInfoPage;
@@ -91,4 +97,80 @@ public class UserCanWorkWithContactTest extends TestBase {
         Assert.assertTrue(contactsPage.isNoResultMessageDisplayed(), "No result message is not visible");
         contactsPage.takeScreenshotNoResultMessage();
     }
+
+    @Epic(value = "Contact")
+    @Feature(value = "User can Add edit delete contact")
+    @Description(value = "User can Add edit delete new contact")
+    @Severity(SeverityLevel.CRITICAL)
+    @AllureId("4")
+    @Test(description = "Work with new contact")
+
+    public void workWithNewContact(){
+
+        String email = "newtest@gmail.com";
+        String password = "newtest@gmail.com";
+        String language = "English";
+
+        String firstName = faker.internet().uuid();
+        String lastName = faker.internet().uuid();
+        String description = faker.lorem().sentence();
+
+        String editFirstName = faker.internet().uuid();
+        String editLastName = faker.internet().uuid();
+        String editDescription = faker.lorem().sentence();
+
+        userApi = new UserApi();
+        String token = userApi.login(email, password, 200);
+
+        contactApi = new ContactApi(token);
+        JsonPath json = contactApi.createContact(201).jsonPath();
+        int contactId = json.getInt("id");
+        app.driver.get("http://phonebook.telran-edu.de:8080/contacts/"+contactId);
+
+        loginPage = new LoginPage(app.driver);
+        loginPage.waitForLoading();
+        loginPage.login(email,password);
+
+        contactsPage = new ContactsPage(app.driver);
+        contactsPage.waitForLoading();
+        contactsPage.selectLanguage(language);
+        Assert.assertEquals(contactsPage.getLanguage(),language);
+
+        addContactDialog = contactsPage.openAddContactDialog();
+        addContactDialog.waitForOpen();
+        addContactDialog.setAddContactForm(firstName,lastName,description);
+        addContactDialog.saveContact();
+
+        // check created contact " Создать новый контакт "
+        contactInfoPage = new ContactInfoPage(app.driver);
+        contactInfoPage.waitForLoading();
+        checkContactData(contactInfoPage,firstName,lastName,description);
+
+        // edit contact " изменить контакт "
+        editContactForm = contactInfoPage.openEditContactForm();
+        editContactForm.waitForOpen();
+        editContactForm.setFirstNameInput(editFirstName);
+        editContactForm.setLastNameInput(editLastName);
+        editContactForm.setDescriptionInput(editDescription);
+        editContactForm.saveChanges();
+
+        //check edited contact
+        contactInfoPage.waitForLoading();
+        checkContactData(contactInfoPage,editFirstName,editLastName,editDescription);
+
+        //open contacts page
+        contactInfoPage.openContactsPage();
+        contactsPage.waitForLoading();
+
+        //filter by contact name
+        contactsPage.filterByContact(editFirstName);
+        contactsPage.waitForLoading();
+
+        //delete contact
+        deleteContactDialog = contactsPage.openDeleteDialog();
+        deleteContactDialog.waitForOpen();
+        deleteContactDialog.setConfirmDeletion();
+        deleteContactDialog.removeContact();
+    }
 }
+
