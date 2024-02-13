b05cdda944c50b3d653a3934194c9c5287c18e22
Georgiy <g.manolov@web.de>
2024-02-13 22:02:57 +0100

    Create New User complete

--- src/test/java/e2e/TestBase.java
+++ src/test/java/e2e/TestBase.java
@@ -3,18 +3,17 @@ package e2e;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import java.net.MalformedURLException;
 
 public class TestBase {
-        protected static ApplicationManager app = new ApplicationManager();
+    protected static ApplicationManager app = new ApplicationManager();
 
-        @BeforeMethod
-        public void setAppTest() {
-            app.init();
-        }
+    @BeforeMethod
+    public void setAppTest() {
+        app.init();
+    }
 
-        @AfterMethod
-    public void tearDown(){
+    @AfterMethod
+    public void tearDown() {
         app.stop();
     }
 }
