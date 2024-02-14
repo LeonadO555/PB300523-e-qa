efd45b9965deb78e10c98373bb041d3f394c6815
nata757 <107883642+nata757@users.noreply.github.com>
2024-02-14 19:45:30 +0100

    added Test user can edit existing contact

--- src/test/java/e2e/UserCanEditExistingContactTest.java
+++ src/test/java/e2e/UserCanEditExistingContactTest.java
@@ -2,50 +2,68 @@ package e2e;
 
 import com.github.javafaker.Faker;
 import e2e.pages.*;
-import e2e.utils.DataProviders;
+import integration.user.UserApi;
+import io.qameta.allure.*;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class UserCanEditExistingContactTest extends TestBase {
+
+    UserApi userApi;
     LoginPage loginPage;
     ContactsPage contactsPage;
     AddContactDialog addContactDialog;
     ContactInfoPage contactInfoPage;
     EditContactForm editContactForm;
-    DeleteContactDialog deleteContactDialog;
-    Faker faker = new Faker();
 
-    private void checkContactData(ContactInfoPage page, String firstName, String lastName, String description) {
+    Faker faker = new Faker();
+    private DeleteContactDialog deleteContactDialog;
+
+    private void checkContactData(ContactInfoPage page, String firstName, String lastName, String contactDescription) {
         String actualFirstName = page.getFirstName();
         String actualLastName = page.getLastName();
-        String actualDescription = page.getDescription();
+        String actualContactDescription = page.getDescription();
         Assert.assertEquals(actualFirstName, firstName, actualFirstName + " is not equal " + firstName);
         Assert.assertEquals(actualLastName, lastName, actualLastName + " is not equal " + lastName);
-        Assert.assertEquals(actualDescription, description, actualDescription + " is not equal " + description);
+        Assert.assertEquals(actualContactDescription, contactDescription, actualContactDescription + " is not equal " + contactDescription);
     }
 
-    @Test(dataProvider = "newContact", dataProviderClass = DataProviders.class)
-    public void userCanWorkWithContactTest(String firstName, String lastName, String description) {
+    @Epic(value = "User with changed data")
+    @Feature(value = "Contact data was changet")
+    @Description(value = " Contact info created")
+    @Severity(SeverityLevel.CRITICAL)
+    @AllureId("3")
+    @Test(description = "Work with edited existing contact")
+    public void EditContact() {
         String email = "chia.dare@hotmail.com";
         String password = "p5evet4u5yow";
         String language = "English";
+        String firstName = "Nata";
+        String lastName = "Koval";
+        String description = "Austria,Wien";
 
         String editFirstName = faker.internet().uuid();
         String editLastName = faker.internet().uuid();
         String editDescription = faker.lorem().sentence();
 
-        // login as user
         loginPage = new LoginPage(app.driver);
         loginPage.waitForLoading();
         loginPage.login(email, password);
 
-        // check that user was logged
         contactsPage = new ContactsPage(app.driver);
         contactsPage.waitForLoading();
         contactsPage.selectLanguage(language);
         Assert.assertEquals(contactsPage.getLanguage(), language);
 
-        // edit contact
+        addContactDialog = contactsPage.openAddContactDialog();
+        addContactDialog.waitForOpen();
+        addContactDialog.setAddContactForm(firstName, lastName, description);
+        addContactDialog.saveContact();
+
+        contactInfoPage = new ContactInfoPage(app.driver);
+        contactInfoPage.waitForLoading();
+        checkContactData(contactInfoPage, firstName, lastName, description);
+
         editContactForm = contactInfoPage.openEditContactForm();
         editContactForm.waitForOpen();
         editContactForm.setFirstNameInput(editFirstName);
@@ -53,8 +71,25 @@ public class UserCanEditExistingContactTest extends TestBase {
         editContactForm.setDescriptionInput(editDescription);
         editContactForm.saveChanges();
 
-        //check edited contact
         contactInfoPage.waitForLoading();
-        checkContactData(contactInfoPage, editFirstName, editLastName,editDescription);
+        checkContactData(contactInfoPage, editFirstName, editLastName, editDescription);
+        contactInfoPage.openContactsPage();
+        contactsPage.waitForLoading();
+
+        contactsPage.filterByContact(editFirstName);
+        contactsPage.waitForLoading();
+
+        int actualContactCountRow = contactsPage.getContactCount();
+        Assert.assertEquals(actualContactCountRow, 1, "Contact count row after filter should be 1");
+
+        deleteContactDialog = contactsPage.openDeleteDialog();
+        deleteContactDialog.waitForOpen();
+        deleteContactDialog.setConfirmDeletion();
+        deleteContactDialog.removeContact();
+
+        Assert.assertTrue(contactsPage.isNoResultMessageDisplayed(), "No result message is not visible");
+        contactsPage.takeScreenshotNoResultMessage();
     }
 }
+
+
